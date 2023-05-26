0221f4e93b40b57342a3c6f88ce6e75d48294adb
same19 <86081703+same19@users.noreply.github.com>
2023-05-26 14:42:44 -0700

    Add files via upload

---
+++ uploadToDrive.py
@@ -0,0 +1,27 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import pandas as pd
+import os
+import glob
+import csv
+from xlsxwriter.workbook import Workbook
+gauth = GoogleAuth()           
+drive = GoogleDrive(gauth)  
+upload_file_list = []
+
+for csvfile in glob.glob(os.path.join('.', '*.csv')):
+    workbook = Workbook(csvfile[:-4] + '.xlsx')
+    upload_file_list.append(csvfile[:-4] + '.xlsx')
+    worksheet = workbook.add_worksheet()
+    with open(csvfile, 'rt', encoding='utf8') as f:
+        reader = csv.reader(f)
+        for r, row in enumerate(reader):
+            for c, col in enumerate(row):
+                worksheet.write(r, c, col)
+    workbook.close()
+
+for upload_file in upload_file_list:
+	gfile = drive.CreateFile({'parents': [{'id': '1zlWyYfzWGGp7_l97s6QKSbOeMFVeIF3O'}]})
+	# Read file and set it as the content of this instance.
+	gfile.SetContentFile(upload_file)
+	gfile.Upload() # Upload the file.
