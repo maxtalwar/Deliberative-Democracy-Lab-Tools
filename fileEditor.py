bd5ce4a031cb405d094fb1f00216e1747d689247
0xphoton <42502920+maxtalwar@users.noreply.github.com>
2023-05-26 14:21:54 -0700

    downloaded example code, added functionality to remove code

--- fileEditor.py
+++ fileEditor.py
@@ -2,25 +2,30 @@ import pandas as pd
 import glob
 import os
 
+# Step 0: Get all CSV file paths in the 'to_be_processed/' directory
 file_paths = glob.glob('to_be_processed/*.csv')
 
 for file_path in file_paths:
+    # Load the data
     df = pd.read_csv(file_path)
 
-    # delete row 1
+    # Step 1: Delete row 1
     df = df.iloc[1:]
 
-    # delete 'id', 'logId', and 'chunkID' columns
+    # Step 2: Delete 'id', 'logId', and 'chunkID' columns
     df = df.drop(columns=['id', 'logId', 'chunkID'], errors='ignore')
 
-    # rename 'userID' to 'speaker' and 'transcriptText' to 'text'
+    # Step 3: Rename 'userID' to 'speaker' and 'transcriptText' to 'text'
     df = df.rename(columns={'userID': 'speaker', 'transcriptText': 'text'})
 
-    # get the filename without the extension
+    # Get the filename without the extension
     filename = os.path.basename(file_path).split('.')[0]
 
-    # define the output file path
+    # Define the output file path
     output_file_path = f'processed/{filename}.xlsx'
 
-    # save the modified DataFrame to an Excel file
+    # Save the modified DataFrame to an Excel file
     df.to_excel(output_file_path, index=False)
+    
+    # Delete the original file
+    os.remove(file_path)
